edf2cecd3f708b5b546bff0817df8f60c8c47dcd
Chirana <engsw.thiago@gmail.com>
2025-01-02 17:24:26 -0300

    fix: adjusting to prod

---
+++ src/components/donate/DonateTableDonationsMonthComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h1>Donaters do Mês</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+})
+</script>
+
+<style></style>
