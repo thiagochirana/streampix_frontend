a46c623e49d5b165af149c6d2de72126d7b82937
Chirana <engsw.thiago@gmail.com>
2025-01-03 20:10:21 -0300

    fix: fixing donate

--- src/services/CableService.ts
+++ src/services/CableService.ts
@@ -2,7 +2,7 @@ import { createConsumer } from '@rails/actioncable'
 
 const CableService = {
   createConsumerDonate(donateId: string) {
-    return createConsumer(`${import.meta.env.VITE_BACKEND_API_URL}/cable?donate_id=${donateId}`)
+    return createConsumer(`${import.meta.env.VITE_BACKEND_WS_URL}/cable?donate_id=${donateId}`)
   },
   createConsumerAlert(alertAccessKey: string) {
     return createConsumer(
